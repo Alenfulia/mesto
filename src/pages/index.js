e9f270f4d25ffb02898fdd6d9b9c69b7148aca67
Alenfulia <alfenkaa@yandex.ru>
2021-12-01 00:50:08 +0300

    sprint_8(webpack)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
-import { Card } from "./Card.js";
-import { FormValidator } from "./FormValidator.js";
-import { initialCards } from "./Utils.js";
+import "../pages/index.css";
+import { Card } from "../components/Card.js";
+import { FormValidator } from "../components/FormValidator.js";
+import { initialCards } from "../components/Utils.js";
 
-import Section from "./Section.js";
-import PopupWithImage from "./PopupWithImage.js";
-import PopupWithForm from "./PopupWithForm.js";
-import UserInfo from "./UserInfo.js";
+import Section from "../components/Section.js";
+import PopupWithImage from "../components/PopupWithImage.js";
+import PopupWithForm from "../components/PopupWithForm.js";
+import UserInfo from "../components/UserInfo.js";
 
 
 const popupEditProfile = document.querySelector('.popup_edit-profile');
